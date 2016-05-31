da4be4be3b0353b0d96d02264710844c69acf7d5
sreimler <soeren@reimler.de>
2016-05-31 12:09:18 -0700

    Added note data object

---
+++ app/src/main/java/com/sreimler/quicknotes/data/Note.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2016 Sören Reimler
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.sreimler.quicknotes.data;
+
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.IgnoreExtraProperties;
+import com.google.firebase.database.ServerValue;
+
+/**
+ * Data object for a single note.
+ */
+@IgnoreExtraProperties
+public class Note {
+
+    private String title;
+    private String text;
+
+    public Note() {
+    }
+
+    public Note(String title, String text) {
+        this.title = title;
+        this.text = text;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
